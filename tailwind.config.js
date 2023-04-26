a972e786c808ad3ba3f6b838cc0a9b012fbf203f
patjennings <patjennings@macbook-pro-de-patjennings-1.home>
2023-04-26 11:57:13 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './**/*.html'
+    ],
+    theme: {
+        screens: {
+            'phone': '480px',
+            'tablet': '640px',
+            'laptop': '960px',
+            'desktop': '1280px',
+        },
+        extend: {
+            backgroundImage: {
+                'header-image': 'url("assets/wave.svg")',
+                'site-texture': ''
+            },
+            spacing: {
+                '128' : '32rem',
+                '192' : '48rem'
+            }
+        },
+        colors: {
+            transparent: 'transparent',
+            'white': '#ffffff',
+            blue: {
+                400: '#367CCF',
+                600: '#2B5485',
+                750: '#1D334E',
+                800: '#0F2743'
+            },
+            orange: {
+                200: '#f8f8ee',
+                600: '#FABA2F'
+            },
+            green: {
+                800: '#23844D',
+                600: '#38C674'
+            },
+            gray: {
+                100: '#EBEAF1',
+                200: '#DCDAE7',
+                300: '#B3B0C1',
+                400: '#706D81',
+                500: '#444251',
+                700: '#26252E',
+                900: '#121216',
+            }
+        }
+    },
+    plugins: [],
+}
+
